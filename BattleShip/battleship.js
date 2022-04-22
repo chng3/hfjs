0b522a1d58a151b6fe2593462464ed862e5e52c2
chngdolphin <chngdolphin@gmail.com>
2022-04-22 10:37:36 +0800

    完成基本逻辑代码

--- BattleShip/battleship.js
+++ BattleShip/battleship.js
@@ -1,35 +1,32 @@
-//存储战舰的位置
+//存储三只战舰的位置
 var location1 = 3;
 var location2 = 4;
 var location3 = 5;
 
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+var guess; //一个存储用户猜测的变量guess
+var hits = 0; //存储击中战舰的次数
+var guesses = 0; //存储猜测次数的变量，计数器
+var isSunk = false; //是否全部击中开关,默认为否false，记录战舰是否被击沉的变量
 
-while(!isSunk){
+while(isSunk == false){
     guess = prompt("Ready, aim, fire! (enter a number 0-6):");
     if (guess<0||guess>6){
         alert("Please enter a valid cell number!");
     }else{
         guesses ++;
-        if(guess == location1){
+        if(guess == location1||guess == location2||guess == location3){
             hits ++;
             alert("hit!");
+            if (hits == 3){
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        }else{
+            alert("Miss!");
         }
-        else if(guess == location2){
-            hits ++;
-            alert("hit!");
-        }
-        else if(guess == location3){
-            hits ++;
-            alert("hit!");
-        }
-        if (hits == 3){
-            isSunk = true;
-            alert("You sank my battleship!"+" and spend " + guesses + " times!");
-        }
-
     }
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+            "which means your shooting accuracy is " + 3/guesses;
+alert(stats);
